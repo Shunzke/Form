f1d51f2f9dd56d2a93fe28e85bd20908451d8473
Shunzke <66575520+Shunzke@users.noreply.github.com>
2023-06-12 10:39:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const mode =document.getElementById("modo-icon");
+mode.addEventListener("click",() => {
+    if (mode.classList.contains("./img/icons8 - lua - cheia - 48.png"))
+    mode.classList.add();
+    return;
+
+}
+ mode.classList.add():
+ if (mode.classList.contains("./img/icons8 - lua - cheia - 48.png"))
+ }}:
